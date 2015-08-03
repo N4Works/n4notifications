5a283e00ec6e336742dc938dda26c282865c7e87
Tiago Resende <tiagor87@gmail.com>
2015-08-03 20:27:33 -0300

    Cria arquivo que concatena os módulos.

--- dist/n4notifications.js
+++ dist/n4notifications.js
@@ -23,6 +23,18 @@
 ;
 (function (ng) {
   ng
+    .module('n4Notifications', [
+      'n4Notifications.directives',
+      'n4Notifications.models',
+      'n4Notifications.services'
+    ])
+}(angular))
+
+"use strict";
+
+;
+(function (ng) {
+  ng
     .module('n4Notifications.directives', ['n4Notifications.models', 'n4Notifications.services'])
     .run([
       '$templateCache',
---
+++ src/n4notifications.js
@@ -0,0 +1,11 @@
+"use strict";
+
+;
+(function (ng) {
+  ng
+    .module('n4Notifications', [
+      'n4Notifications.directives',
+      'n4Notifications.models',
+      'n4Notifications.services'
+    ])
+}(angular))
