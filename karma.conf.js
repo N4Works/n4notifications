803800f4f4fb6d2ab48ec55d829c4422a7cf9bf8
Tiago Resende <tiagor87@gmail.com>
2015-08-03 11:14:29 -0300

    Acerto nas configurações de teste.

--- karma.conf.js
+++ karma.conf.js
@@ -35,7 +35,9 @@ var _karma = function(config)
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'src/n4Notifications.js': 'coverage'
+      'src/n4notificationmodel.js': 'coverage',
+      'src/n4notificationservice.js': 'coverage',
+      'src/n4notificationsdirective.js': 'coverage'
     },
 
     coverageReporter:
