3c756901279d5c31ea20b4211bed7a6ac2bcc0a5
Tiago Resende <tiagor87@gmail.com>
2015-11-18 15:26:58 -0200

    Adiciona arquivo de definição.

---
+++ typing/n4notificationsservice.d.ts
@@ -0,0 +1,8 @@
+declare module n4Notifications {
+    export interface N4NotificationsService {
+        notify(type:string, message:string, primaryButtonText:string, secondaryButtonText:string, callback?:(selected:string) => void):Promise<string>;
+        notifySuccess(message:string, primaryButtonText:string, secondaryButtonText:string, callback?:(selected:string) => void):Promise<string>;
+        notifyInformation(message:string, primaryButtonText:string, callback?:(selected:string) => void):Promise<string>;
+        notifyAlert(message:string, primaryButtonText:string, secondaryButtonText:string, callback?:(selected:string) => void):Promise<string>;
+    }
+}
