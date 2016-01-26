916e5453792c1957d52edc6f2cb95d9e68d3217d
Tiago Resende <tiagor87@gmail.com>
2016-01-26 15:56:33 -0200

    Remove dependencia do ngAnimation

--- dist/n4notifications.js
+++ dist/n4notifications.js
@@ -25,7 +25,6 @@
 
   ng
     .module("n4Notifications", [
-      "ngAnimate",
       "n4Notifications.directives",
       "n4Notifications.models",
       "n4Notifications.services"
--- src/n4notifications.js
+++ src/n4notifications.js
@@ -4,7 +4,6 @@
 
   ng
     .module("n4Notifications", [
-      "ngAnimate",
       "n4Notifications.directives",
       "n4Notifications.models",
       "n4Notifications.services"
